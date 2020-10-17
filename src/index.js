e5749e3609decd9852e0cdc04de01c7d7754456f
varunkumarnr <varunnrk@gmail.com>
2020-10-17 12:15:08 +0530

    firbase connection

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import { render } from "react-dom";
 import App from "./App";
 import "normalize.css";
 import { GlobalStyles } from "./global-styles";
-
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
