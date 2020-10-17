4c875db10f05fff71e6f9cba7e5c6e04e00d1509
varunkumarnr <varunnrk@gmail.com>
2020-10-17 14:16:00 +0530

    sign in page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components/index";
+import FooterContainer from "../containers/Footer";
 const Signin = () => {
-  return <p> this is Signin page </p>;
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+  const isInValid = password === "" || emailAddress === "";
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error data-testid='error'>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method='POST'>
+            <Form.Input
+              placeholder='Email address'
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type='password'
+              value={password}
+              autoComplete='off'
+              placeholder='Password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isInValid}
+              type='submit'
+              data-testid='sign-in'
+            >
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to='/signup'>Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+
+      <FooterContainer />
+    </>
+  );
 };
 export default Signin;
