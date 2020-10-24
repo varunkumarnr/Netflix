cf3769ca285a480453d346c3a87a0212f6e51447
varunkumarnr <varunnrk@gmail.com>
2020-10-24 12:49:14 +0530

    selection page

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -1,8 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 export function BrowseContainer({ slides }) {
+  const [profile, setprofile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
-  return <SelectProfileContainer user={user} />;
+
+  useEffect(() => {
+    // console.log("profile", profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+  return <SelectProfileContainer user={user} setProfile={setprofile} />;
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -17,7 +17,14 @@ export function SelectProfileContainer({ user, setProfile }) {
       <Profiles>
         <Profiles.Title>Who's Watching</Profiles.Title>
         <Profiles.List>
-          <Profiles.User onClick={() => {}}>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
             <Profiles.Picture src={user.photoURL} />
             <Profiles.Name>{user.displayName} </Profiles.Name>
           </Profiles.User>
